b90a51ed587731cf9f03439d8ce5352b39e4d8ae
Brandon Richardson <64274517+brich33713@users.noreply.github.com>
2020-11-13 17:29:03 -0500

    Add files via upload

---
+++ giphy.js
@@ -0,0 +1,21 @@
+$('#submit').bind('click', function(e) {
+	e.preventDefault();
+	// console.log({$('input').val()};)
+	getParameters($('input').val(), 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym');
+});
+
+$('#remove').bind('click', function(e) {
+	e.preventDefault();
+	$('div img').remove();
+});
+
+async function getParameters(q, api_key) {
+	try {
+		let x = await axios.get('http://api.giphy.com/v1/gifs/search', { params: { q, api_key } }); //"api_key" : "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"}})
+		console.log(x.data.data[0].images);
+		if (x.data.data[0].url) {
+			$('#imageHolder').append('<img>');
+			$('img').attr('src', `${x.data.data[0].images.original.url}`);
+		}
+	} catch (e) {}
+}
